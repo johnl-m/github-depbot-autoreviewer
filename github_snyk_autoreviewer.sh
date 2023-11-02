fd5c79f5511fe3ab640cf8307f680d2c4c1204ba
John Lund-Molfese <24286589+johnl-m@users.noreply.github.com>
2023-11-02 17:43:30 -0500

    Update github_snyk_autoreviewer.sh

--- github_snyk_autoreviewer.sh
+++ github_snyk_autoreviewer.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+getopts 
+if [[ ! -n $1 ]];
+then 
+    echo "No snyk user passed. Usage: ./github_snyk_autoreviewer.sh your_org_snyk_account_name"
+    exit 1
+fi
+echo "Received snyk user $1"
+SNYK_USER="$1"
 if ! command -v gh &> /dev/null
 then
     echo "gh command not found. Make sure you've installed the GitHub CLI!"
     exit 1
 fi
 GH_USER=$(gh api user | jq -r '.login')
-PRS=$(gh search prs --assignee $GH_USER --author snyk-vestwell --state open --json repository,number)
+PRS=$(gh search prs --assignee $GH_USER --author $SNYK_USER --state open --json repository,number)
 jq -c '.[]' <<< "$PRS" | while read i; do
 REPO_NAME=$(jq -r <<< $i '.repository.nameWithOwner')
 PR_NUMBER=$(jq -r <<< $i '.number')
